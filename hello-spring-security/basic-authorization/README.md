f78303c8b3d0cd787efecd60426d836c1a4541db
Hoseop.kim <kimho314@naver.com>
2025-03-23 00:10:44 +0900

    [feat] : add basic authentication configuration

---
+++ hello-spring-security/basic-authorization/README.md
@@ -0,0 +1,6 @@
+## Basic Authentication
+
+- http header를 사용하는 인증 방ㅂ버 중 하나
+- <usernmae>:<password> 로 이루어진 문자열을 base64로 인코딩 후 Authorication header에 실어서 서버에 전송
+
+![basic-authentication](./basic_authentication.png)
---
+++ hello-spring-security/basic-authorization/src/main/java/com/example/basicauthorization/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.basicauthorization.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+//    private final CustomAuthenticationProvider authenticationProvider;
+//
+//    public SecurityConfig(CustomAuthenticationProvider authenticationProvider) {
+//        this.authenticationProvider = authenticationProvider;
+//    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+            .httpBasic(Customizer.withDefaults())
+            .formLogin(Customizer.withDefaults())
+            .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated());
+//            .authenticationProvider(authenticationProvider);
+
+        return http.build();
+    }
+}
