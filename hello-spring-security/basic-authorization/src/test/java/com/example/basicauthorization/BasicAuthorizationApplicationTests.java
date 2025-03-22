d96bf3d8888f3f013a275ec00107ab89cefe872d
Hoseop.kim <kimho314@naver.com>
2025-03-22 19:48:09 +0900

    [feat] : init hello-spring-security project

---
+++ hello-spring-security/basic-authorization/src/test/java/com/example/basicauthorization/BasicAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basicauthorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicAuthorizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
