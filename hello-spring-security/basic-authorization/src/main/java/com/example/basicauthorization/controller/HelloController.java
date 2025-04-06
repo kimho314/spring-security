1551a14fdd6f205472b13baa24760b1247c8d0a5
Hoseop.kim <kimho314@naver.com>
2025-04-06 20:04:02 +0900

    [feat] : add helloSecurityContextHolder controller, helloAuthenticationParameter controller

--- hello-spring-security/basic-authorization/src/main/java/com/example/basicauthorization/controller/HelloController.java
+++ hello-spring-security/basic-authorization/src/main/java/com/example/basicauthorization/controller/HelloController.java
@@ -1,5 +1,8 @@
 package com.example.basicauthorization.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +11,18 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         return "hello";
+    }
+
+    @GetMapping("/hello/security-context-holder")
+    public String helloSecurityContextHolder(){
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        Authentication authentication = securityContext.getAuthentication();
+
+        return "Hello " + authentication.getName();
+    }
+
+    @GetMapping("/hello/authentication-parameter")
+    public String helloAuthenticationParameter(Authentication authentication){
+        return "Hello " + authentication.getName();
     }
 }
