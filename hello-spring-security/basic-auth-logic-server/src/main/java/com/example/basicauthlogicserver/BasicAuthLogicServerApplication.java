070b46503535e851e902e16bd14d74afe34ad6f2
Hoseop.Kim <kimho314@gmail.com>
2025-06-28 13:20:01 +0300

    [feat]: add basic-auth-logic-server module

---
+++ hello-spring-security/basic-auth-logic-server/src/main/java/com/example/basicauthlogicserver/BasicAuthLogicServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.basicauthlogicserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicAuthLogicServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicAuthLogicServerApplication.class, args);
+    }
+
+}
---
+++ hello-spring-security/basic-auth-logic-server/src/test/java/com/example/basicauthlogicserver/BasicAuthLogicServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basicauthlogicserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicAuthLogicServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
