40432857764569779c01a222e8b90c4b1678eb3a
Hoseop.Kim <kimho314@gmail.com>
2025-09-12 21:16:09 +0900

    [feat]: init spring oauth2 auth server project

---
+++ spring-oauth2-auth-server/src/test/java/com/eazybytes/springoauth2authserver/SpringOauth2AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eazybytes.springoauth2authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauth2AuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
