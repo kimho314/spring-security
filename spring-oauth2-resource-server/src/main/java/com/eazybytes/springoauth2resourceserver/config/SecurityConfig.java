69d8e8ce75b73f387088b68cabf11b6cdaa6771d
Hoseop.Kim <kimho314@gmail.com>
2025-09-26 15:59:54 +0300

    [feat]: enhance SecurityConfig with jwt configuration and update DemoController mapping

---
+++ spring-oauth2-resource-server/src/main/java/com/eazybytes/springoauth2resourceserver/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.eazybytes.springoauth2resourceserver.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Value(value = "${keySetUri}")
+    private String keySetUri;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.oauth2ResourceServer(
+                c -> c.jwt(
+                        j -> j.jwkSetUri(keySetUri) // configuring the public key set URI
+                )
+        );
+
+        http.authorizeHttpRequests(
+                c -> c.anyRequest().authenticated()
+        );
+
+        return http.build();
+    }
+}
--- spring-oauth2-resource-server/src/main/java/com/eazybytes/springoauth2resourceserver/controller/DemoController.java
+++ spring-oauth2-resource-server/src/main/java/com/eazybytes/springoauth2resourceserver/controller/DemoController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
-    @GetMapping(value = "/demo")
+    @GetMapping(value = "/demo", produces = "application/json")
     public String demo(){
         return "demo";
     }
