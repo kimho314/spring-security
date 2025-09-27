8dabc19c5e21fb646790242cd2b9b4abcee7c6ef
Hoseop.Kim <kimho314@gmail.com>
2025-09-27 19:02:40 +0300

    [feat]: implement combined JWT and opaque token authentication support in SecurityConfig

--- spring-oauth2-resource-server/src/main/java/com/eazybytes/springoauth2resourceserver/config/SecurityConfig.java
+++ spring-oauth2-resource-server/src/main/java/com/eazybytes/springoauth2resourceserver/config/SecurityConfig.java
@@ -4,10 +4,17 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationManagerResolver;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.server.resource.authentication.JwtIssuerAuthenticationManagerResolver;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationProvider;
+import org.springframework.security.oauth2.server.resource.authentication.OpaqueTokenAuthenticationProvider;
+import org.springframework.security.oauth2.server.resource.introspection.OpaqueTokenIntrospector;
+import org.springframework.security.oauth2.server.resource.introspection.SpringOpaqueTokenIntrospector;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -42,7 +49,12 @@ public class SecurityConfig {
 
         // configuring multitenancy
         http.oauth2ResourceServer(
-                c -> c.authenticationManagerResolver(authenticationManagerResolver())
+                c -> c.authenticationManagerResolver(
+                        authenticationManagerResolver(
+                                jwtDecoder(),
+                                opaqueTokenIntrospector()
+                        )
+                )
         );
 
         http.authorizeHttpRequests(
@@ -53,10 +65,41 @@ public class SecurityConfig {
     }
 
     @Bean
-    public AuthenticationManagerResolver<HttpServletRequest> authenticationManagerResolver() {
-        return JwtIssuerAuthenticationManagerResolver.fromTrustedIssuers(
-                "http://localhost:9000",
-                "http://localhost:9001"
+    public AuthenticationManagerResolver<HttpServletRequest> authenticationManagerResolver(
+            JwtDecoder jwtDecoder,
+            OpaqueTokenIntrospector opaqueTokenIntrospector
+    ) {
+        // Defining an authentication manager for the authorization server managing JWT access tokens
+        AuthenticationManager jwtAuth = new ProviderManager(
+                new JwtAuthenticationProvider(jwtDecoder)
         );
+        // Defining another authentication manager for the authorization server managing opaque tokens
+        AuthenticationManager opaqueAuth = new ProviderManager(
+                new OpaqueTokenAuthenticationProvider(opaqueTokenIntrospector)
+        );
+        
+        return (request) -> {
+            if ("jwt".equals(request.getHeader("type"))) {
+                return jwtAuth;
+            }
+            else {
+                return opaqueAuth;
+            }
+        };
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder
+                .withJwkSetUri("http://localhost:9000/oauth2/jwks")
+                .build();
+    }
+
+    @Bean
+    public OpaqueTokenIntrospector opaqueTokenIntrospector() {
+        return SpringOpaqueTokenIntrospector.withIntrospectionUri("http://localhost:9001/oauth2/introspect")
+                .clientId("credential-client2")
+                .clientSecret("secret3")
+                .build();
     }
 }
