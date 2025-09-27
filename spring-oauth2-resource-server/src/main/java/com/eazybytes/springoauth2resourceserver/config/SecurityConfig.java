c839e178734b5f58ab267c1f47594f022336d911
Hoseop.Kim <kimho314@gmail.com>
2025-09-27 17:29:44 +0300

    [feat]: add multi-tenancy support through JwtIssuerAuthenticationManagerResolver in SecurityConfig

--- spring-oauth2-resource-server/src/main/java/com/eazybytes/springoauth2resourceserver/config/SecurityConfig.java
+++ spring-oauth2-resource-server/src/main/java/com/eazybytes/springoauth2resourceserver/config/SecurityConfig.java
@@ -1,10 +1,13 @@
 package com.eazybytes.springoauth2resourceserver.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManagerResolver;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtIssuerAuthenticationManagerResolver;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -34,13 +37,26 @@ public class SecurityConfig {
 //        );
 
         // configuring the credentials the resource server must use to authenticate when calling the authorization server’s introspection URI
-        http.oauth2ResourceServer(c -> c.opaqueToken(o -> o.introspectionUri(introspectionUri)
-                .introspectionClientCredentials(resourceServerClientID, resourceServerSecret)));
+//        http.oauth2ResourceServer(c -> c.opaqueToken(o -> o.introspectionUri(introspectionUri)
+//                .introspectionClientCredentials(resourceServerClientID, resourceServerSecret)));
+
+        // configuring multitenancy
+        http.oauth2ResourceServer(
+                c -> c.authenticationManagerResolver(authenticationManagerResolver())
+        );
 
         http.authorizeHttpRequests(
                 c -> c.anyRequest().authenticated()
         );
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManagerResolver<HttpServletRequest> authenticationManagerResolver() {
+        return JwtIssuerAuthenticationManagerResolver.fromTrustedIssuers(
+                "http://localhost:9000",
+                "http://localhost:9001"
+        );
     }
 }
