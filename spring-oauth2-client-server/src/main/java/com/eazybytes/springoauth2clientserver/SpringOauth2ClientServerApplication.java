36f495b6f01b54f80a34c9f2e70f52f4169447de
Hoseop.Kim <kimho314@gmail.com>
2025-09-30 16:37:15 +0300

    [feat]: init spring oauth2 client server project

---
+++ spring-oauth2-client-server/src/main/java/com/eazybytes/springoauth2clientserver/SpringOauth2ClientServerApplication.java
@@ -0,0 +1,13 @@
+package com.eazybytes.springoauth2clientserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauth2ClientServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOauth2ClientServerApplication.class, args);
+    }
+
+}
---
+++ spring-oauth2-client-server/src/test/java/com/eazybytes/springoauth2clientserver/SpringOauth2ClientServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eazybytes.springoauth2clientserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauth2ClientServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
