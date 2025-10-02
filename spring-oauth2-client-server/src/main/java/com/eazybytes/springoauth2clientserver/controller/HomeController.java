3f759650d1e292c667a6833be9408ca616abc309
Hoseop.Kim <kimho314@gmail.com>
2025-10-02 11:44:51 +0300

    [feat]: log OAuth2 authentication details in HomeController

--- spring-oauth2-client-server/src/main/java/com/eazybytes/springoauth2clientserver/controller/HomeController.java
+++ spring-oauth2-client-server/src/main/java/com/eazybytes/springoauth2clientserver/controller/HomeController.java
@@ -1,12 +1,29 @@
 package com.eazybytes.springoauth2clientserver.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String home() {
+    public String home(OAuth2AuthenticationToken authentication) {
+        log.info("auth: {}", authentication);
+        String info = """
+                credentials: %s
+                name: %s
+                attributes: %s
+                authorities: %s
+                registrationId: %s
+                """.formatted(authentication.getCredentials().toString(),
+                authentication.getPrincipal().getName().toString(),
+                authentication.getPrincipal().getAttributes().toString(),
+                authentication.getPrincipal().getAuthorities().toString(),
+                authentication.getAuthorizedClientRegistrationId().toString());
+        log.info(info);
+
         return "index.html";
     }
 }
