3b9c70fcb9ed43a268018a78120a53e80bbae178
Hoseop.Kim <kimho314@gmail.com>
2025-10-02 11:26:49 +0300

    [feat]: add Google OAuth2 client registration with InMemoryClientRegistrationRepository

--- spring-oauth2-client-server/src/main/java/com/eazybytes/springoauth2clientserver/config/SecurityConfig.java
+++ spring-oauth2-client-server/src/main/java/com/eazybytes/springoauth2clientserver/config/SecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,5 +20,20 @@ public class SecurityConfig {
         http.authorizeHttpRequests(c -> c.anyRequest().authenticated());
 
         return http.build();
+    }
+
+    // customize providers
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        return new InMemoryClientRegistrationRepository(
+                googleClientRegistration()
+        );
+    }
+
+    private ClientRegistration googleClientRegistration() {
+        return CommonOAuth2Provider.GOOGLE.getBuilder("google")
+                .clientId("910420895131-doog22k8j8pt435jji81vkj6851hc6l8.apps.googleusercontent.com")
+                .clientSecret("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")
+                .build();
     }
 }
