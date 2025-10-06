42a42f2b4a5d618a70bf8819358013a8a0d6cd57
Hoseop.Kim <kimho314@gmail.com>
2025-10-06 16:53:29 +0300

    [feat]: split demo and hello controllers; update security config with GET /hello authentication

--- spring-reactive-security/src/main/java/com/eazybytes/springreactivesecurity/config/SecurityConfig.java
+++ spring-reactive-security/src/main/java/com/eazybytes/springreactivesecurity/config/SecurityConfig.java
@@ -2,21 +2,33 @@ package com.eazybytes.springreactivesecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
     @Bean
-    public ReactiveUserDetailsService userDetailsService(){
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http.httpBasic(Customizer.withDefaults());
+        http.authorizeExchange(
+                c -> c.pathMatchers(HttpMethod.GET, "/hello") // Selects there quests for which we apply the authorization rules
+                        .authenticated()
+                        .anyExchange().permitAll()
+        );
+        return http.build();
+    }
+
+    @Bean
+    public ReactiveUserDetailsService userDetailsService() {
         var u = User.withUsername("kim")
                 .password("{noop}12345")
                 .authorities("read")
--- spring-reactive-security/src/main/java/com/eazybytes/springreactivesecurity/controller/DemoController.java
+++ spring-reactive-security/src/main/java/com/eazybytes/springreactivesecurity/controller/DemoController.java
@@ -1,16 +1,14 @@
 package com.eazybytes.springreactivesecurity.controller;
 
 import org.springframework.http.MediaType;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
 public class DemoController {
-    @GetMapping(value = "/hello", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Mono<String> demo(Mono<Authentication> auth){
-        Mono<String> message = auth.map(a -> "Hello " + a.getName());
-        return message;
+    @GetMapping(value = "/demo", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<String> demo() {
+        return Mono.just("Hello");
     }
 }
