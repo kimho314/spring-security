c1e424dc8e36daabe9b8bd92d1fb1b9babedd551
Hoseop.Kim <kimho314@gmail.com>
2025-10-06 17:27:08 +0300

    [feat]: implements custom authorization logic for /hello endpoint with time-based and role-based access control

--- spring-reactive-security/src/main/java/com/eazybytes/springreactivesecurity/config/SecurityConfig.java
+++ spring-reactive-security/src/main/java/com/eazybytes/springreactivesecurity/config/SecurityConfig.java
@@ -2,36 +2,82 @@ package com.eazybytes.springreactivesecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authorization.AuthorizationContext;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalTime;
+import java.util.function.Function;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
+//    @Bean
+//    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+//        http.httpBasic(Customizer.withDefaults());
+//        http.authorizeExchange(
+//                c -> c.pathMatchers(HttpMethod.GET, "/hello") // Selects there quests for which we apply the authorization rules
+//                        .authenticated()
+//                        .anyExchange().permitAll()
+//        );
+//        return http.build();
+//    }
+
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         http.httpBasic(Customizer.withDefaults());
-        http.authorizeExchange(
-                c -> c.pathMatchers(HttpMethod.GET, "/hello") // Selects there quests for which we apply the authorization rules
-                        .authenticated()
-                        .anyExchange().permitAll()
+
+        http.authorizeExchange(c -> c.anyExchange()
+                .access(this::getAuthorizationDecisionMono) // configuring a custom authorization strategy
         );
+
         return http.build();
+    }
+
+    private Mono<AuthorizationDecision> getAuthorizationDecisionMono(
+            Mono<Authentication> a,
+            AuthorizationContext c
+    ) {
+        String path = getRequestPath(c);
+        boolean restrictedTime = LocalTime.now().isBefore(LocalTime.NOON);
+
+        if (path.equals("/hello")) {
+            return a.map(isAdmin())
+                    .map(auth -> auth && !restrictedTime)
+                    .map(AuthorizationDecision::new);
+        }
+        return Mono.just(new AuthorizationDecision(false));
+    }
+
+    private String getRequestPath(AuthorizationContext c) {
+        return c.getExchange()
+                .getRequest()
+                .getPath()
+                .toString();
+    }
+
+    private Function<Authentication, Boolean> isAdmin() {
+        return p ->
+                p.getAuthorities().stream()
+                        .anyMatch(e -> e.getAuthority().equals("ROLE_ADMIN"));
     }
 
     @Bean
     public ReactiveUserDetailsService userDetailsService() {
         var u = User.withUsername("kim")
                 .password("{noop}12345")
-                .authorities("read")
+                .authorities(new SimpleGrantedAuthority("ROLE_ADMIN"))
                 .build();
         var uds = new MapReactiveUserDetailsService(u);
         return uds;
