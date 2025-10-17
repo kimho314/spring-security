5322e89fe21c23c109374d39bcea016113179d1f
Hoseop.Kim <kimho314@gmail.com>
2025-10-17 19:16:44 +0300

    [feat]: add helloAuthenticatedWithMockUserDetails

--- hello-spring-security/basic-security-test/src/main/java/com/eazybytes/basicsecuritytest/config/SecureConfig.java
+++ hello-spring-security/basic-security-test/src/main/java/com/eazybytes/basicsecuritytest/config/SecureConfig.java
@@ -1,19 +1,50 @@
 package com.eazybytes.basicsecuritytest.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecureConfig {
-//    @Bean
-//    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-//        http.csrf(csrf -> csrf.disable())
-//                .cors(cors -> cors.disable())
-//                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
-//                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
-//
-//        return http.build();
-//    }
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.disable())
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .exceptionHandling(ex -> ex.authenticationEntryPoint((request, response, authException) -> response.setStatus(HttpStatus.UNAUTHORIZED.value()))
+                        .accessDeniedHandler((request, response, accessDeniedException) -> response.setStatus(HttpStatus.FORBIDDEN.value())))
+        ;
 
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+
+        UserDetails user1 = User.withUsername("john")
+                .password("{noop}12345")
+                .authorities("READ")
+                .build();
+
+        UserDetails user2 = User.withUsername("jane")
+                .password("{noop}12345")
+                .authorities("WRITE")
+                .build();
+
+        manager.createUser(user1);
+        manager.createUser(user2);
+
+        return manager;
+    }
 }
--- hello-spring-security/basic-security-test/src/test/java/com/eazybytes/basicsecuritytest/controller/HelloControllerTest.java
+++ hello-spring-security/basic-security-test/src/test/java/com/eazybytes/basicsecuritytest/controller/HelloControllerTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
@@ -40,6 +41,13 @@ public class HelloControllerTest {
         mvc.perform(get("/hello")
                         .with(user("mary")))
                 .andExpect(content().string("hello"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithUserDetails("john")
+    public void helloAuthenticatedWithMockUserDetails() throws Exception {
+        mvc.perform(get("/hello"))
                 .andExpect(status().isOk());
     }
 }
