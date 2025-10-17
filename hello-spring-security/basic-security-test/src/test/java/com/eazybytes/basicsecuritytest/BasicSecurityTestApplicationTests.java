a93c7f6ce5e1bde737cd0ddcf2fe070c8c1c34ae
Hoseop.Kim <kimho314@gmail.com>
2025-10-17 12:43:12 +0300

    [feat]: add basic security test module

---
+++ hello-spring-security/basic-security-test/src/test/java/com/eazybytes/basicsecuritytest/BasicSecurityTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eazybytes.basicsecuritytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSecurityTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
